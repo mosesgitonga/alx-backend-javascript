1d5fa856d8f0e99506e4f8194eaa317bda908a74
mosesgitonga <infosec947@gmail.com>
2024-01-04 17:03:34 +0300

    commented code

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,33 +1,35 @@
-export default class {
+export default class Currency {
     constructor(code, name) {
         this.code = code;
         this.name = name;
     }
 
     get code() {
-        return this.code
+        return this._code;
     }
 
     set code(dCode) {
         if (typeof dCode !== 'string') {
-            throw new TypeError("code should be string")
+            throw new TypeError("Code should be a string");
         }
         this._code = dCode;
     }
 
     get name() {
-        return this._code;
+        return this._name;
     }
 
     set name(newName) {
         if (typeof newName !== 'string') {
-            throw new TypeError('name should be string');
+            throw new TypeError('Name should be a string');
         }
-
         this._name = newName;
     }
 
     displayFullCurrency() {
-        return `${this._name} (${this._code})`
+        
+        return `${this._name} (${this._code})`;
     }
+
+
 }
