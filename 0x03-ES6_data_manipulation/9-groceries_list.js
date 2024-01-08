2434566407769ba3dbb839f9059f52f07ab331df
mosesgitonga <infosec947@gmail.com>
2024-01-08 16:25:13 +0300

    created map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,16 @@
+import { group } from "console";
+
+function groceriesList() {
+    const groceries = [
+        ['Apples', '10'],
+        ['Tomatoes', '10'],
+        ['Pasta', '1'],
+        ['Rice', '1'],
+        ['Banana', '5']
+    ]
+
+    const groceriesMap = new Map(groceries)
+    return groceriesMap;
+}
+
+export default groceriesList;
