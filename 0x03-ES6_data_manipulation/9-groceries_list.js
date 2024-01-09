660c8d00d74cfff639f74e97574b6eb29434f089
mosesgitonga <infosec947@gmail.com>
2024-01-09 12:09:38 +0300

    values not strings

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -2,11 +2,11 @@ import { group } from "console";
 
 function groceriesList() {
     const groceries = [
-        ['Apples', '10'],
-        ['Tomatoes', '10'],
-        ['Pasta', '1'],
-        ['Rice', '1'],
-        ['Banana', '5']
+        ['Apples', 10],
+        ['Tomatoes', 10],
+        ['Pasta', 1],
+        ['Rice', 1],
+        ['Banana', 5]
     ]
 
     const groceriesMap = new Map(groceries)
