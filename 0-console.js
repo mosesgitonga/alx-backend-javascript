f618c54eb99fcec786d7e0db4df1f8b9e2783f0f
mosesgitonga <infosec947@gmail.com>
2024-02-26 18:35:18 +0300

    test checker with comments

--- 0-console.js
+++ 0-console.js
@@ -1,5 +1,11 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
 const displayMessage = (msg) => {
     console.log(msg);
   };
   
-module.exports = displayMessage;
+  module.exports = displayMessage;
+  
