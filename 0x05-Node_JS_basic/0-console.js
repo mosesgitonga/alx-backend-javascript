7c1773e26945ce25613874d4743ad4bfe4e2540e
mosesgitonga <infosec947@gmail.com>
2024-02-26 19:57:44 +0300

    comments

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} strArg The message to display.
+ * @author mosesgitonga AKA root <infosec947@gmail.com>
+ */
 function displayMessage(strArg) {
     console.log(strArg)
 }
