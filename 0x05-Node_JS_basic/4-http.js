64ae1ac78fd071b5bced1f2d05678a2c725ed216
mosesgitonga <infosec947@gmail.com>
2024-02-27 15:45:20 +0300

    rename server to app and export app

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,12 +2,14 @@ const http = require('http');
 
 const PORT = 1245;
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   res.end('Hello Holberton School!');
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log('server is Active');
 });
+
+module.exports = app;
