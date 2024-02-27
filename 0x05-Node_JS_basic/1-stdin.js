e2b61b979e7a7e73db2f8d2c484419fc4e093678
mosesgitonga <infosec947@gmail.com>
2024-02-27 07:34:17 +0300

    taking input from commandline

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+/**
+ * ask for input from the commandline
+ */
+
+const readline = require('readline');
+
+const r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+r1.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+  r1.close();
+});
+
+r1.on('close', () => {
+  console.log('This important software is now closing');
+});
