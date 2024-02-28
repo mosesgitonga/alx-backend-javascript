3ec7e03d5693511c1917eaa952a6374add49f5a1
mosesgitonga <infosec947@gmail.com>
2024-02-28 15:44:18 +0300

    use expect from chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,18 +1,17 @@
-const chai = require('chai');
-const assert = chai.assert;
+const expect = require('chai').expect;
 const calculateNumber = require('./1-calcul')
 
 describe('combining description', function() {
     it('type SUM', function() {
-        assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+        expect(calculateNumber('SUM', 1.4, 4.5), 6);
     });
     it('type SUBTRACT', function() {
-        assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4)
+        expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4)
     });
     it('type DIVIDE', function() {
-        assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+        expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
     })
     it('type DIVIDE', function() {
-        assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+        expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     })
 })
