97f8b6f5bb49e6263dfa1e6cc2e28c9bfd8ff933
mosesgitonga <infosec947@gmail.com>
2024-03-01 11:53:47 +0300

    contains deprication warning

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,11 +1,11 @@
 const express = require('express')
-app = express()
+const app = express()
 
 app.get('/', (_, res) => {
     res.send('Welcome to the payment system')
 })
 
-const PORT = 3000
+const PORT = 7865
 app.listen(PORT, () => {
     console.log('API available on localhost port 7865')
 })
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,14 +1,14 @@
 const request = require('request')
-const app = require('./api')
+//const app = require('./api')
 const { expect } = require('chai');
 
 describe('index page', () => {
-    const  api_url = 'http://localhost:3000'
-    it('should return status code 200', async() => {
-        request.get(`${api_url}`,(_err, resp, body) => {
+    const  api_url = 'http://localhost:7865'
+    it('should return status code 200', (done) => {
+        request.get(`${api_url}/`,(_err, res, body) => {
             expect(res.statusCode).to.equal(200);
             expect(res.body).to.equal('Welcome to the payment system');
-            done();
+            done()
         });
     });
 })
