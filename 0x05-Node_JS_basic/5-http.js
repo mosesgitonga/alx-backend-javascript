86abd1d64eaaf8dad4b1a0390bc56c8bf2f7f8a5
mosesgitonga <infosec947@gmail.com>
2024-03-02 17:09:08 +0300

    async

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,55 +1,46 @@
 const http = require('http');
-const fs = require('fs');
+const { readFile } = require('fs');
 
-const PORT = 1245;
+const hostname = '127.0.0.1';
+const port = 1245;
 
-function countStudents(dbFilePath) {
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
   return new Promise((resolve, reject) => {
-    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+    readFile(fileName, (err, data) => {
       if (err) {
-        console.error('Cannot load the database');
         reject(err);
-        return;
-      }
-
-      let lines = data.split('\n');
-      lines = lines.filter((line) => line.trim() !== '');
-
-      const numberOfStudents = lines.length - 1;
-      console.log(`Number of students: ${numberOfStudents}`);
-
-      let firstNameCs = '';
-      let firstNameSwe = '';
-      let CsCount = 0;
-      let SweCount = 0;
-
-      for (let i = 1; i < numberOfStudents + 1; i += 1) {
-        if (lines[i]) {
-          const student = lines[i];
-
-          const firstName = student.split(',')[0];
-          const field = student.split(',')[3];
-
-          if (field === 'CS') {
-            CsCount += 1;
-            firstNameCs += `${firstName}, `;
-          } else if (field === 'SWE') {
-            SweCount += 1;
-            firstNameSwe += `${firstName}, `;
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
           }
         }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
       }
-
-      firstNameCs = firstNameCs.slice(0, -2);
-      firstNameSwe = firstNameSwe.slice(0, -2);
-
-      const output = `Number of students: ${numberOfStudents}\n`+
-                      `Number of students in CS: ${CsCount}. List: ${firstNameCs}\n` +
-                     `Number of students in SWE: ${SweCount}. List: ${firstNameSwe}`;
-
-      console.log(output);
-
-      resolve(output); // Resolve with the output
     });
   });
 }
@@ -64,7 +55,8 @@ const app = http.createServer((req, res) => {
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
     countStudents(process.argv[2].toString()).then((output) => {
-      res.end(output); // Send the output to the client
+      const outString = output.slice(0, -1);
+      res.end(outString);
     }).catch(() => {
       res.statusCode = 404;
       res.end('Cannot load the database');
@@ -72,8 +64,7 @@ const app = http.createServer((req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log('Server is active');
+app.listen(port, hostname, () => {
 });
 
-module.export = app
+module.exports = app;
